6aa1fed588b2758d91bfab9cee8d31eb95637f6f
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2020-06-23 15:12:02 -0400

    demo - useAlgolia + MUI/lab/autocomplete

--- stories/omni-search.stories.js
+++ stories/omni-search.stories.js
@@ -1,9 +1,14 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 import { storiesOf } from "@storybook/react"
 import StoryRouter from 'storybook-react-router'
 
+import Autocomplete from '@material-ui/lab/Autocomplete'
+import TextField from '@material-ui/core/TextField'
+
 import OmniSearch from "../src/mui-search-bar"
+import useAlgolia from '../src/algolia-hook'
+
 
 storiesOf("OmniSearch", module)
   .addDecorator(StoryRouter())
@@ -20,5 +25,52 @@ storiesOf("OmniSearch", module)
     >
       <OmniSearch />
     </div>
-  )
-  )
+  ))
+  .add('NomniSearch', () => {
+    const [value, setValue] = useState(null)
+    const [inputValue, setInputValue] = useState('')
+    const [options, setOptions] = useState([])
+    const specs = {
+      hitsPerPage: 50,
+      getRankingInfo: true,
+      restrictSearchableAttributes: ['name', 'id'],
+      exactOnSingleWordQuery: 'word',
+    }
+
+    const { debouncedSearch, results } = useAlgolia({
+      searchAPIkey: process.env.REACT_APP_ALGOLIA_SEARCH_KEY,
+      specs,
+    })
+
+    useEffect(() => {
+      if (inputValue) {
+        debouncedSearch(inputValue)
+      }
+    }, [inputValue])
+
+    useEffect(() => {
+      setOptions(results)
+    }, [results])
+
+    return (
+      <Autocomplete
+        id="grouped-demo"
+        options={options}
+        autoComplete
+        filterSelectedOptions
+        includeInputInList
+        value={value}
+        groupBy={(option) => option.level}
+        getOptionLabel={(option) => `${option.id} - ${option.name}`}
+        style={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="With categories" variant="outlined" />}
+        filterOptions={(x) => x}
+        onChange={(_, newValue) => {
+          setValue(newValue)
+        }}
+        onInputChange={(_, newInputValue) => {
+          setInputValue(newInputValue)
+        }}
+      />
+    )
+  })
